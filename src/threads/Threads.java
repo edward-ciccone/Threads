e1c9398f148f7fa687bfbd75b99fc1c5f130ac81
edward <edward@edward-linux-mint>
2016-08-26 02:30:17 -0500

    added static method to display some properties that can be used to show threads

--- src/threads/Threads.java
+++ src/threads/Threads.java
@@ -6,6 +6,8 @@
 package threads;
 
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -16,42 +18,45 @@ public class Threads {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws InterruptedException {
+        // Delay, in milliseconds before interruption (default 1 hour)
+        long patience = 1000 * 60 * 60;
+        
+        // If cmd line arg present, gives patience in seconds
+        if (args.length > 0) {
+            try {
+                patience = Long.parseLong(args[0]) * 1000;
+            } catch (NumberFormatException e) {
+                System.err.println("Argument must be an integer");
+                System.exit(1);
+            }
+        }
+        
         Runnable r1 = new Runnable() {
+            @Override
             public void run() {
+                System.out.println("Haaaa");
                 try {
-                    while (true) {
-                        System.out.println("Hello, world!");
-                        Thread.sleep(1000L);
-                    }
-                } catch (InterruptedException iex) {
+                    Thread.sleep(2000L);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Threads.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
-        };
-        Runnable r2 = new Runnable() {
-            public void run() {
-                try {
-                    while (true) {
-                        System.out.println("Goodbye, "
-                                + "cruel world!");
-                        Thread.sleep(2000L);
-                    }
-                } catch (InterruptedException iex) {
-                }
-            }
+            
         };
         
-        Thread thr1 = new Thread(r1);
-        Thread thr2 = new Thread(r2);
-        thr1.start();
+        Thread t1 = new Thread(r1);
+        t1.start();
         
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Message: ");
-        String message = sc.nextLine();
-        System.out.println("MESSAGE = " + message);
-        sc.close();
+        threadMessage("Starting MessageLoop thread");
+    }
+    
+    static void threadMessage(String message) {
+        String threadName = Thread.currentThread().getName();
+        int threadCount = Thread.activeCount();
         
+        System.out.format("%s: %s%n", threadName, message);
+        System.out.format("%s: %s%n", threadName, threadCount);
     }
 
 }
